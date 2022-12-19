f27d7498daf1df08d76a505b584b188e05da7599
Oleksii Umrikhin <89519243+Novohudo@users.noreply.github.com>
2022-12-19 19:08:20 +0100

    Basic structure

---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css'
+
+const CellComponent = () => {
+    return (
+        <div className={'cell'}>
+
+        </div>
+    );
+};
+
+export default CellComponent;
